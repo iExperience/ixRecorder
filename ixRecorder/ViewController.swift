9f88b6c8c43bf7f264a427aaf7a03dc3b6e2e2da
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2017-06-27 14:59:38 +0200

    My new files

---
+++ ixRecorder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ixRecorder
+//
+//  Created by Miki von Ketelhodt on 2017/06/27.
+//  Copyright © 2017 RBG Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("This is a view controller")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
